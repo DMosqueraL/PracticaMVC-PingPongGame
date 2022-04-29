6f2ac8a8177b6b4fe4b8a87654418b3f815f937d
DMosqueral <dml2410@gmail.com>
2022-04-28 21:54:14 -0500

    Se agregaron las barras al canvas

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Modelo
 (function(){
     self.Board = function(width, height) {
         this.width = width;
@@ -10,14 +11,39 @@
 
     self.Board.prototype = {
         get elements(){
-            let elements = this.bars;
-            elements.push(ball);
+            var elements = this.bars;
+            elements.push(this.ball);
             return elements;
         }
     }
 
 })();
 
+(function(){
+    self.Bar = function(x, y, width, height, board){
+        this.x= x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+        this.board.bars.push(this);
+        this.kind = "rectangle";
+        
+    }
+
+    self.Bar.prototype={
+        down: function(){
+            
+        },
+        up: function(){
+
+        }
+
+    }
+
+})();
+
+//Vista
 (function(){
     self.BoardView = function(canvas, board){
         this.canvas = canvas;
@@ -26,12 +52,43 @@
         this.board = board;
         this.ctx = canvas.getContext("2d");
     }
+
+    self.BoardView.prototype = {
+        
+        draw: function(){
+            
+            for(var i = this.board.elements.length -1; i>=0; i--){
+                var el = this.board.elements[i];
+
+                draw(this.ctx, el)
+            };
+        }
+    }
+
+    function draw(ctx,element){
+        if(element !== null && element.hasOwnProperty("kind")){
+            switch(element.kind){
+                case "rectangle":
+                    console.log("Hola mundo");
+                    ctx.fillRect(element.x, element.y, element.width, element.height);
+                    break;
+            }
+        }
+    }
+
+        
 })();
 
-window.addEventListener("load", main)
+self.addEventListener("load", main)
 
+//Controlador
 function main (){
-    const board = new Board(800,600);
-    const canvas = document.getElementById("canvas");
-    const board_view = new BoardView(canvas, board);
+    
+    var board = new Board(300,400);
+    var bar = new Bar(20, 100, 40, 100, board);
+    var bar = new Bar(800, 100, 40, 100, board);
+    var canvas = document.getElementById('canvas');
+    var board_view = new BoardView(canvas, board);   
+
+    board_view.draw();
 }

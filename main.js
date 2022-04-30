67cfd0004b356b1b3d015f9e86511bd2065f4f41
DMosqueral <dml2410@gmail.com>
2022-04-29 19:20:01 -0500

    Se finaliza al 100% el código de la práctica

--- main.js
+++ main.js
@@ -12,11 +12,11 @@
 
   self.Board.prototype = {
     get elements() {
-      var elements = this.bars.map(function (bar) {return bar;});
+      var elements = this.bars.map(function (bar) { return bar; });
       elements.push(this.ball);
       return elements;
     }
-  };
+  }
 })();
 
 (function () {
@@ -28,20 +28,42 @@
     this.speed_y = 0;
     this.speed_x = 3;
     this.direction = 1;
+    this.bounce_angle = 0;
+    this.max_bounce_angle = Math.PI / 12;
+    this.speed = 3;
 
     board.ball = this;
     this.kind = "circle";
-    
   }
 
   self.Ball.prototype = {
     move: function () {
-      this.x += (this.speed_x*thid.direction);
+      this.x += (this.speed_x * thid.direction);
       this.y += (this.speed_y);
+    },
+    get width() {
+      return this.radius * 2;
+    },
+    get height() {
+      return this.radius * 2;
+    },
+    collision: function (bar) {//Esta función calcula el ángulo en el cual va a moverse la pelota, cambiar dirección
+      var relative_intersect_y = (bar.y + (bar.height / 2)) - this.y;
+
+      var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+
+      this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+
+      this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+      this.speed_x = this.speed * Math.cos(this.bounce_angle);
+
+      if (this.x > (this.board.width / 2)) this.direction = -1;
+      else this.direction = 1;
     }
   }
+})();
 
-})(function () {
+(function () {
   self.Bar = function (x, y, width, height, board) {
     this.x = x;
     this.y = y;
@@ -51,7 +73,7 @@
     this.board.bars.push(this);
     this.kind = "rectangle";
     this.speed = 10;
-  };
+  }
 
   self.Bar.prototype = {
     down: function () {
@@ -62,8 +84,8 @@
     },
     toString: function () {
       return "x: " + this.x + " y: " + this.y;
-    },
-  };
+    }
+  }
 })();
 
 //Vista
@@ -74,7 +96,7 @@
     this.canvas.height = board.height;
     this.board = board;
     this.ctx = canvas.getContext("2d");
-  };
+  }
 
   self.BoardView.prototype = {
     clean: function () {
@@ -87,14 +109,43 @@
         draw(this.ctx, el);
       };
     },
+    check_collisions: function () {
+      for (var i = this.board.bars.length - 1; i >= 0; i--) {
+        var bar = this.board.bars[i];
+        if (hit(bar, this.board.ball)) {
+          this.board.ball.collision(bar);
+        }
+      }
+    },
     play: function () {
-      if(this.board.playing){
+      if (this.board.playing) {
         this.clean();
         this.draw();
+        this.check_collisions();
         this.board.ball.move();
       }
     }
-  };
+  }
+
+  function hit(a, b) { // Esta función verifica si a colisiona con b        
+    var hit = false;
+
+    if (b.x + b.width >= a.x && b.x < a.x + a.width) {
+      if (b.y + b.height >= a.y && b.y < a.y + a.height)
+        hit = true;
+    }
+    if (b.x <= a.x && b.x + b.width >= a.x + a.width) {
+
+      if (b.y <= a.y && b.y + b.height >= a.y + a.height)
+        hit = true;
+    }
+
+    if (a.x <= b.x && a.x + a.width >= b.x + b.width) {
+      if (a.y <= b.y && a.y + a.height >= b.y + b.height)
+        hit = true;
+    }
+    return hit;
+  }
 
   function draw(ctx, element) {
     switch (element.kind) {
@@ -115,7 +166,7 @@
 var board = new Board(800, 400);
 var bar = new Bar(20, 100, 40, 100, board);
 var bar_2 = new Bar(800, 100, 40, 100, board);
-var canvas = document.getElementById("canvas");
+var canvas = document.getElementById('canvas');
 var board_view = new BoardView(canvas, board);
 var ball = new Ball(350, 100, 10, board);
 
@@ -134,7 +185,7 @@ document.addEventListener("keydown", function (ev) {
   } else if (ev.key === "KeyS") {
     ev.preventDefault();
     bar_2.down();
-  } else if(ev.key === "Space"){
+  } else if (ev.key === "Space") {
     ev.preventDefault();
     board.playing = !board.playing;
   }
